9c3771ba2ce04602bd19b6059d4c3bce8f740035
aromero64 <108192385+aromero64@users.noreply.github.com>
2022-06-25 15:48:06 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+
+$(".button-one ").click(function() {
+let UserReview=$(".Review").val();
+console.log(UserReview);   
+    
+    alert(UserReview);
+    $(".reviews").text(UserReview);
+    
+    
+  
+    
+    
+    
+});
+
+
